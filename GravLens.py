4f8a6974a723c20b4ffeb7a6cc4bc93550870365
baklanovp <baklanovp@gmail.com>
2019-10-23 19:13:04 +0300

    - FIX: refactor code

--- GravLens.py
+++ GravLens.py
@@ -1,166 +1,155 @@
 from tkinter import *
 from math import *
 
-#-------massive coordinates of object------#
+# -------massive coordinates of object------#
 
-#objectx=[0, 1, 0, -1]
-#objecty=[1, 0, -1, 0]
+# objectx=[0, 1, 0, -1]
+# objecty=[1, 0, -1, 0]
 
-#---------------********------------------#
+# ---------------********------------------#
 
 
+# -------coordinates of circle object------#
+objectx = []
+objecty = []
 
-#-------coordinates of circle object------#
-objectx=[]
-objecty=[]
+numpoint = 100
+alpha0 = 2 * 3.14 / numpoint
+radius = 0.03
+# for pcr in range(numpoint):
+#    alpha=alpha0*pcr
+#    objectx.append(radius*cos(alpha)+0.1)
+#    objecty.append(radius*sin(alpha)+0.1)
 
-numpoint=100
-alpha0=2*3.14/numpoint
-radius=0.03
-##for pcr in range(numpoint):
-##    alpha=alpha0*pcr
-##    objectx.append(radius*cos(alpha)+0.1)
-##    objecty.append(radius*sin(alpha)+0.1)
-
-radius=0.02
+radius = 0.02
 for pcr in range(numpoint):
-    alpha=alpha0*pcr
-    objectx.append(radius*cos(alpha)+0.1)
-    objecty.append(radius*sin(alpha)+0.1)    
-#------------------********----------------#
+    alpha = alpha0 * pcr
+    objectx.append(radius * cos(alpha) + 0.1)
+    objecty.append(radius * sin(alpha) + 0.1)
+# ------------------********----------------#
+
+# objectx=[]
+# objecty=[]
+# numberline=40
+# numberpoint=640
+# for ln in range(numberline):
+#    for point in range(numberpoint):
+#        objectx.append((ln-10.2)/19)
+#        #objectx.append((ln*4/numberpoint-2))
+#        objecty.append((point*4/numberpoint-2))
+# print(objectx)
+# print(objecty)
 
 
+# -------double lenses------#
 
-##objectx=[]
-##objecty=[]
-##numberline=40
-##numberpoint=640
-##for ln in range(numberline):
-##    for point in range(numberpoint):
-##        objectx.append((ln-10.2)/19)
-##        #objectx.append((ln*4/numberpoint-2))
-##        objecty.append((point*4/numberpoint-2))
-##print(objectx)
-##print(objecty)
+# lensposx=[0.2, -0.2, 0, -0.5]
+# lensposy=[0.01, 0.01, 0.3, -0.5]
 
+# --------********---------#
+
+# -------single lense------#
+
+lensposx = [0.001]
+lensposy = [0]
 
 
-
-#-------double lense------#
-
-##lensposx=[0.2, -0.2, 0, -0.5]
-##lensposy=[0.01, 0.01, 0.3, -0.5]
-
-#--------********---------#
-
-#-------single lense------#
-
-lensposx=[0.001]
-lensposy=[0]
-   
-#--------********---------#
+# --------********---------#
 
 
+# ---------------------------------------------------------Calculation angle of deflaction----------------------------------------------#
 
 
-
-#---------------------------------------------------------Calculation angle of deflaction----------------------------------------------#
-
-
-def mvec(x, y):                       #modul of vector  
-    return (x**2 + y**2)**0.5   
-
-def theta(m, p):                      #function of deflection angle
-    return -0.009*m/p
-
-vsumx=0.0
-vsumy=0.0
+def mvec(x, y):  # modul of vector
+    return (x ** 2 + y ** 2) ** 0.5
 
 
-vecsumx=[]
-vecsumy=[]
+def theta(m, p):  # function of deflection angle
+    return -0.009 * m / p
+
+
+vsumx = 0.0
+vsumy = 0.0
+
+vecsumx = []
+vecsumy = []
 
 for objects in range(len(objectx)):
     for lens in range(len(lensposx)):
-        if lens==1:
-            m=10
-        else: m=1
-        vecx=(lensposx[lens]-objectx[objects])/mvec((lensposx[lens]-objectx[objects]),(lensposy[lens]-objecty[objects]))*theta(m, mvec((lensposx[lens]-objectx[objects]),(lensposy[lens]-objecty[objects])))
-        vecy=(lensposy[lens]-objecty[objects])/mvec((lensposx[lens]-objectx[objects]),(lensposy[lens]-objecty[objects]))*theta(m, mvec((lensposx[lens]-objectx[objects]),(lensposy[lens]-objecty[objects])))
+        if lens == 1:
+            m = 10
+        else:
+            m = 1
+        vecx = (lensposx[lens] - objectx[objects]) / \
+               mvec((lensposx[lens] - objectx[objects]), (lensposy[lens] - objecty[objects])) * \
+               theta(m, mvec((lensposx[lens] - objectx[objects]), (lensposy[lens] - objecty[objects])))
+        vecy = (lensposy[lens] - objecty[objects]) / \
+               mvec((lensposx[lens] - objectx[objects]), (lensposy[lens] - objecty[objects])) * \
+               theta(m, mvec((lensposx[lens] - objectx[objects]), (lensposy[lens] - objecty[objects])))
 
-        vsumx=vsumx+vecx
-        vsumy=vsumy+vecy
+        vsumx = vsumx + vecx
+        vsumy = vsumy + vecy
 
+    vecsumx.append(vsumx + objectx[objects])
+    vecsumy.append(vsumy + objecty[objects])
+    vsumx = 0.0
+    vsumy = 0.0
+# ---------------------------------------------------------*********************************---------------------------------------------#
 
-    vecsumx.append(vsumx+objectx[objects])
-    vecsumy.append(vsumy+objecty[objects])
-    vsumx=0.0
-    vsumy=0.0
-#---------------------------------------------------------*********************************---------------------------------------------#
-
-#----------------------------setcollor----------------------
-##    colormarker=[]
-##    colormarker2=[]
-##    color0=16
-##    coldist=256/(len(objectx)+16)
-##    for col in range(len(objectx)):
-##        color0+=coldist
-##        new0=hex(int(color0))
-##        colormarker.append("#"+new0[2:]+"00ff")
-##        colormarker2.append("#ff"+new0[2:]+"00")
-##        
-#------------------------***************-------------------
-
-
-
-
-#---------------------------------------------------------DRAWING----------------------------------------------#
-
-    
-
-size=800                                                        #size canvas
-root = Tk()                                                     #create window
-canvas = Canvas(root, bg="black", width=size, height=size)      #create canvas
-canvas.pack()                                                   #to place canvas into window
+# ----------------------------setcollor----------------------
+#    colormarker=[]
+#    colormarker2=[]
+#    color0=16
+#    coldist=256/(len(objectx)+16)
+#    for col in range(len(objectx)):
+#        color0+=coldist
+#        new0=hex(int(color0))
+#        colormarker.append("#"+new0[2:]+"00ff")
+#        colormarker2.append("#ff"+new0[2:]+"00")
+#
+# ------------------------***************-------------------
 
 
+# ---------------------------------------------------------DRAWING----------------------------------------------#
 
-zoom=2920                                                         #mashtab                                             
 
-#-------------------------------------Drawing image---------------------------------------#
+size = 800  # size canvas
+root = Tk()  # create window
+canvas = Canvas(root, bg="black", width=size, height=size)  # create canvas
+canvas.pack()  # to place canvas into window
+
+zoom = 2920  # mashtab
+
+# -------------------------------------Drawing image---------------------------------------#
 
 for image in range(len(vecsumx)):
-    d=10
-    x0=vecsumx[image]*zoom+size/2
-    y0=vecsumy[image]*zoom+size/2
-    #colm=(colormarker[image])
-    colm='red'
-    canvas.create_oval(x0, y0, x0+d/2, y0+d/2, fill=colm)        #draw ellips
+    d = 10
+    x0 = vecsumx[image] * zoom + size / 2
+    y0 = vecsumy[image] * zoom + size / 2
+    # colm=(colormarker[image])
+    colm = 'red'
+    canvas.create_oval(x0, y0, x0 + d / 2, y0 + d / 2, fill=colm)  # draw ellips
     root.update()
-#-------------------------------------Drawing lenses--------------------------------------#
+# -------------------------------------Drawing lenses--------------------------------------#
 for lens in range(len(lensposx)):
-    d=8
-    x0=lensposx[lens]*zoom+size/2
-    y0=lensposy[lens]*zoom+size/2
+    d = 8
+    x0 = lensposx[lens] * zoom + size / 2
+    y0 = lensposy[lens] * zoom + size / 2
     print(lensposx[lens])
-    canvas.create_oval(x0, y0, x0+d/2, y0+d/2, fill="white")        #draw ellips
+    canvas.create_oval(x0, y0, x0 + d / 2, y0 + d / 2, fill="white")  # draw ellips
     root.update()
 
-#-------------------------------------Drawing objects-------------------------------------#
+# -------------------------------------Drawing objects-------------------------------------#
 for objects in range(len(objectx)):
-    d=7
-    x0=objectx[objects]*zoom+size/2
-    y0=objecty[objects]*zoom+size/2
-    #colm=(colormarker2[objects])
-    colm='green'
-    canvas.create_oval(x0, y0, x0+d/2, y0+d/2, fill=colm)        #draw ellips
+    d = 7
+    x0 = objectx[objects] * zoom + size / 2
+    y0 = objecty[objects] * zoom + size / 2
+    # colm=(colormarker2[objects])
+    colm = 'green'
+    canvas.create_oval(x0, y0, x0 + d / 2, y0 + d / 2, fill=colm)  # draw ellips
     root.update()
 
-#-----------------------------------********************************--------------------------------------------#    
+# -----------------------------------********************************--------------------------------------------#
 
 
-
-
-
-
-#a=input()
+# a = input()
